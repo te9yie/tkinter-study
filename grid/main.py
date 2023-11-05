4ddf6e760bdaf6952f2f7e7e6d184c93d882bac0
te9yie <te9yie@users.noreply.github.com>
2023-11-05 12:07:40 +0900

    add grid

---
+++ grid/main.py
@@ -0,0 +1,42 @@
+import tkinter
+
+
+def create_frame(parent, text):
+    frame = tkinter.Frame(parent)
+    label = tkinter.Label(frame, text=text)
+    label.pack()
+    return frame
+
+
+def main():
+    root = tkinter.Tk()
+    root.geometry("320x240")
+
+    tkinter.Label(root, text="LABEL 1", borderwidth=1, relief="raised").grid(
+        row=0, column=0
+    )
+    tkinter.Label(root, text="LABEL 2", borderwidth=1, relief="raised").grid(
+        row=0, column=1, sticky=tkinter.EW
+    )
+    tkinter.Label(root, text="LABEL 3", borderwidth=1, relief="raised").grid(
+        row=0, column=2
+    )
+    tkinter.Label(root, text="LABEL 4", borderwidth=1, relief="raised").grid(
+        row=1, column=0, columnspan=2, sticky=tkinter.EW
+    )
+    tkinter.Label(root, text="LABEL 5", borderwidth=1, relief="raised").grid(
+        row=2, column=1, sticky=tkinter.NSEW
+    )
+
+    root.grid_columnconfigure(0, weight=0)
+    root.grid_columnconfigure(1, weight=1)
+    root.grid_columnconfigure(2, weight=0)
+    root.grid_rowconfigure(0, weight=0)
+    root.grid_rowconfigure(1, weight=0)
+    root.grid_rowconfigure(2, weight=1)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
